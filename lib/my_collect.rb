b662991e59e910c1dc81ab84be90732f0822086c
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-12-04 15:38:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(array)
+  if block_given?
+    new_array = []
+    i = 0 
+    while i < array.length
+       yield array[i]
+       new_array << array[i]
+       i += 1 
+    end
+    new_array
+  else
+    return "This block should not run!"
+  end
+end
 
 
