e5feb5150f7667ca07167b73248322803365c8d5
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-12-05 14:34:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(array)
+  if block_given?
+    new_array = []
+    i = 0 
+    while i < array.length
+      if array[i] == array[i].upcase
+       yield array[i]
+       new_array << array[i].downcase
+     end
+       i += 1 
+    end
+    new_array
+  else
+    return "This block should not run!"
+  end
+end
+
+
 
 
