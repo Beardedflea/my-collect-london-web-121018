959498dfca6a58419e035525332f7328e2fa3009
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-12-05 14:53:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,25 @@
+def my_collect(array)
+  if block_given?
+    new_array = []
+    i = 0 
+    while i < array.length
+    yield array[i]
+    
+    if array[i].upcase
+      new_array << array[i].split(" ").first
+    
+    #yield array[i]
+    #else
+     # new_array << array[i].upcase
+    end
+    i += 1 
+    end
+    new_array
+  else
+    return "This block should not run!"
+  end
+end
+
+
 
 
